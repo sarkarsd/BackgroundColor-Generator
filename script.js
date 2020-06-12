4f6eb22d4b4d688bdd015281375ad4205fc78bff
SSarkar <43643705+sarkarsd@users.noreply.github.com>
2020-06-12 07:17:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+css.textContent = "linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")" + ";";
+
+
+function setGradient() {
+	console.log(color1);
+	console.log(color2);
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+//color1.addEventListener("input", setGradient);
+
+//color2.addEventListener("input", setGradient);
